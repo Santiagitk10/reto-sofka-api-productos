302536e235216228fc6a50cbf4a7162c5df3a7cf
Santiago Sierra Herrera <santiago.sierra@sofka.local>
2023-01-02 14:40:38 -0500

    Initial Commit

---
+++ reto-sofka-api-productos/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace reto_sofka_api_productos
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
